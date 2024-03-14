f83c726a6481d6fcc6c875c6384074b1eb3de011
MrToastMachine <alexfomahony@gmail.com>
2024-03-14 23:41:58 +0000

    interrupt plotting

--- Data_processing/interrupt_viewer.py
+++ Data_processing/interrupt_viewer.py
@@ -7,7 +7,15 @@ data_file = "ESP_Output.xlsx"
 data = pd.read_excel(data_file)["datapoints"]
 zeroes = np.zeros(len(data))
 
-plt.figure()
+scaled_data = (data+500) / 1000
 
-plt.plot(data,zeroes, 'o')
+plt.figure()
+plt.plot(scaled_data,zeroes, 'g.')
+plt.xlim(-3,30)
+plt.title("ESP32 Interrupt Trigger Timings", fontsize=18)
+plt.xlabel("Time (ms)",fontsize=12, x=1.0)
+plt.ylabel("Interrupt triggers",fontsize=14)
+plt.legend(["Interrupt Triggered"])
+plt.yticks([])
+plt.grid()
 plt.show()
